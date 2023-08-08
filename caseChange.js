a71532d043ff7fa8d3b11080d5093bcd71223d8a
Utso Sarkar <87900549+utso-sarkar@users.noreply.github.com>
2023-08-08 19:59:23 +0600

    loop solved

---
+++ caseChange.js
@@ -0,0 +1,11 @@
+const username="blackpink";
+const userInput ="BlackpInk";
+console.log(username.toLowerCase());
+console.log(userInput.toLowerCase());
+if(username.toLowerCase()===userInput.toLowerCase()){
+    console.log("Valid User");
+}
+else{
+    console.log("Invalid User");
+    console.log(object);
+}
---
+++ searchItem.js
@@ -0,0 +1,7 @@
+const lyrics ="Tumi bondu kala paki ami jano ki bosonto kale tomay vulte pari ni ";
+const SearchStaring ="paki";
+// const lyricsLower =lyrics.toLowerCase();
+// const SearchStaringLower=SearchStaring.toLowerCase();
+const doesExitOneLine=lyrics.toLowerCase().includes(SearchStaring.toLowerCase());
+console.log(doesExitOneLine);
+
